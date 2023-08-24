2cdb5807a4de727f24d5cb53e7dea820c5388bf5
Prabin Kumar Sahu <78588455+Prabin-Kumar-Sahu@users.noreply.github.com>
2023-08-24 19:19:53 +0530

    next previous button  add

--- src/components/News.js
+++ src/components/News.js
@@ -1,53 +1,92 @@
-import React, { Component, useState } from "react";
+import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 
 export class News extends Component {
-    articles= [
-        {
-          "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-          "author": null,
-          "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-          "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-          "publishedAt": "2020-04-27T11:41:47Z",
-          "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-        },
-        {
-          "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-          "author": null,
-          "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-          "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-          "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-          "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-          "publishedAt": "2020-03-30T15:26:05Z",
-          "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-        }
-      ]
-    constructor()
-{
+  constructor() {
     super();
-    this.state ={
-       article: this.articles
+    this.state = {
+      articles: [],
+      loading: false,
+      page: 1,
+    };
+  }
+  async componentDidMount() {
+    let url =
+      "https://newsapi.org/v2/top-headlines?country=in&apiKey=3ab48aae71b0439c849a0e8a555217ad&page=1";
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles: parsedData.articles,
+      totalResults: parsedData.totalResults,
+    });
+  }
+  handlePrevClick = async () => {
+    let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3ab48aae71b0439c849a0e8a555217ad&page=${
+      this.state.page - 1
+    }&pageSize=20`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    console.log(parsedData);
+    this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+    });
+  };
 
+  handleNextClick = async () => {
+    if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+    } else {
+      let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=3ab48aae71b0439c849a0e8a555217ad&page=${
+        this.state.page + 1
+      }&pageSize=20`;
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      console.log(parsedData);
+      this.setState({
+        page: this.state.page + 1,
+        articles: parsedData.articles,
+      });
     }
-}
+  };
 
   render() {
-
     return (
       <div className="container my-3">
         <h2>News Hunt - Top Headlines</h2>
         <div className="row">
-          <div className="col-md-3">
-            <NewsItem title="hello " description="My description" imageUrl="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg"/>
-          </div>
-          <div className="col-md-3">
-            <NewsItem title="hello " description="My description" />
-          </div>
-          <div className="col-md-3">
-            <NewsItem title="hello " description="My description " />
-          </div>
+          {this.state.articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title.slice(0, 49) : " "}
+                  description={
+                    element.description ? element.description.slice(0, 88) : " "
+                  }
+                  imageUrl={element.urlToImage}
+                  newsUrl={element.url}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div className="container d-flex justify-content-between">
+          <button
+            disabled={this.state.page <= 1}
+            type="button"
+            class="btn btn-primary "
+            onClick={this.handlePrevClick}
+          >
+            {" "}
+            &larr; <span></span>Previous
+          </button>
+          <button
+            type="button"
+            class="btn btn-primary"
+            onClick={this.handleNextClick}
+          >
+            Next <span></span>&rarr;
+          </button>
         </div>
       </div>
     );
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,18 +3,18 @@ import React, { Component } from "react";
 export class NewsItem extends Component {
   render() {
 
-    let {title , description , imageUrl} = this.props;
+    let {title , description , imageUrl,newsUrl} = this.props;
     return (
         <div className= "my-3">
 
-      <div className="card" style={{width: "18rem"}}>
+      <div className="card" style={{width: "18rem"}} >
         <img src={imageUrl} className="card-img-top" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
+          <h5 className="card-title">{title}...</h5>
           <p className="card-text">
-           {description}
+           {description}...
           </p>
-          <a href="http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again" className="btn btn-sm btn-primary">
+          <a rel="noreferrer" href={newsUrl} target="blank" className="btn btn-sm btn-dark">
             Read More
           </a>
         </div>
